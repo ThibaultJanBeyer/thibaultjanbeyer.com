88536718653e7b120fb43b323ed0ec61d821231a
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2016-04-28 11:23:27 +0200

    update img, delete parallax on img add parallax on text, add possibility for custom values in parallax

--- src/assets/javascript/parallaxscroll.js
+++ src/assets/javascript/parallaxscroll.js
@@ -13,13 +13,14 @@ function inView(windowScrollTop, windowScrollLeft) {
 
   // runs a for loop on every element executing what is in the function 
   forEach(elements, function (index, element) {
-
-    // stores the position values of the current element  
+    // get custom speed
+    var dataSpeed = element.getAttribute('data-scroll--speed') || 100;
+    // stores the position values of the current element
     var rect = element.getBoundingClientRect();
     // if the bottom position of the element is higher than 0, thus in view
     if( rect.bottom >= 0 ) {
       if ( element.classList.contains("scroll--permanent") === true ) {
-        element.style.transform = "translate3D(0, " + -windowScrollTop/180 + "%, 0)";
+        element.style.transform = "translate3D(0, " + -windowScrollTop/dataSpeed + "%, 0)";
       } else {
         element.classList.add("scroll--start");
       }
